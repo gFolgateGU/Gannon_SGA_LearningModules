d0738d55434264fdbeec6a17615fd4c37f9848ee
gFolgateGU <gfolgate@outlook.com>
2017-12-25 23:02:18 -0500

    Surgery to IndividualEngageU.html
    Made it mock the structure of ITS_FAQ.html

--- scripts/view_models/individualEngageU/individualViewModel.js
+++ scripts/view_models/individualEngageU/individualViewModel.js
@@ -7,6 +7,9 @@ var individualViewModel = function () {
         hideFeatures();
         var jQueryDivToShow = "#" + name;
         $(jQueryDivToShow).show();
+        window.location.hash = name;
+        var $navHeightBuffer = 0 - $("#mainNav").height();
+        window.scrollBy(0, $navHeightBuffer * 2);
     }
 
     function initFeatures() {
@@ -22,6 +25,8 @@ var individualViewModel = function () {
     }
 
     function hideFeatures() {
+        $("#defaultNoOptionSelectedArea").hide();
+
         vm.features().forEach(function (feat) {
             var jQueryDivHolder = "#" + feat.name;
             $(jQueryDivHolder).hide();
